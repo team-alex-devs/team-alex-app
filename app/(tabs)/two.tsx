4d0ac530966a735ac24b4a0017772110b5b6c600
cainsusk <cain@susko.ca>
2023-10-27 21:19:32 -0400

    feature: add state to a button

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,15 +1,18 @@
 import { StyleSheet } from 'react-native';
+import React, {useState} from 'react';
 
 import IntroScreen from '../../components/IntroScreen';
 import { Text, View } from '../../components/Themed';
 import ImageInput from '../../components/ImageInput';
 
 export default function TabTwoScreen() {
+  const [text, setText] =
+      useState("Text will appear below...\n\n")
   return (
     <View style={styles.container}>
       <Text style={styles.title}>team-alex-app</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <ImageInput/>
+      <ImageInput textOutput={text} setText={setText} />
     </View>
   );
 }
--- components/ImageInput.tsx
+++ components/ImageInput.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button } from 'react-native';
 
 import { Text, View } from './Themed';
 
-import { launchImageLibrary } from 'react-native-image-picker';
-import { string } from 'prop-types';
-import { Asset } from 'expo-asset';
+export default function ImageInput({textOutput, setText}: {
+    textOutput: string,
+    setText: React.Dispatch<React.SetStateAction<string>>
+}) {
+  return <View>
+    <Button
+      title='Upload Images'
+      onPress={() => setText(textOutput + "hello\n")}
+    />
+    <Text>
+      {textOutput}
+    </Text>
+  </View>;
+}
 
-async function query(filename: string) {
+// TODO: add hugging-face api functionality
+async function query(filename: string): Promise<string> {
   const data = new Blob([filename], {type: "image"});
   const response = await fetch(
     'https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large',
@@ -17,41 +29,5 @@ async function query(filename: string) {
       body: data
     }
   );
-  const result = await response.json();
-  return result;
-}
-
-export default function ImageInput() {
-  let text: string = "responses will load here...\n"
-  return <View>
-    <Button
-      title={'Upload Images'}
-      onPress={() => text = text + "hello\n"
-        /* THIS CODE DOES NOT WORK YET
-        launchImageLibrary({ mediaType: 'photo' }, ((response) => {
-            switch (response.assets) {
-              case undefined:
-                return text + "error\n"
-              default:
-                response.assets?.forEach((pic) => {
-                    let uri = pic.uri;
-                    switch (uri) {
-                      case undefined:
-                        return text + "error\n";
-                      default :
-                        return text + query(uri) + "\n";
-                    }
-                  }
-                )
-            }
-          }
-          )
-        )
-         */
-      }
-    />
-    <Text>
-      {text}
-    </Text>
-  </View>;
+  return response.json();
 }
