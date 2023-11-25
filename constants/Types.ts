7172beba94e9e13fad64b769cbf3222c4a44ec47
cainsusk <cain@susko.ca>
2023-11-25 18:59:54 -0500

    feature: add data access layer
    Adds functionality for the app to put and get data from firebase.

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,1 +1,35 @@
-export type SignUpData = {name: string, phoneNum: string};
+/**
+ * This file contains all the data-types used in the application.
+ * Change these types as needed -- but ensure that nothing breaks when you do
+ */
+
+/**
+ * User data
+ */
+export type User = {
+    name: string,
+    phoneNumber: string,
+    password: string,
+    styles: string[]
+};
+export function emptyUser(): User {
+    return {name: "", phoneNumber: "", password: "", styles: []}
+}
+
+/**
+ * Clothing data (which maps to a Image)
+ */
+export type Style = {
+    articleType: string,
+    baseColour: string,
+    gender: string,
+    masterCategory: string,
+    productDisplayName: string,
+    season: string,
+    subcategory: string,
+    usage: string,
+    year: number
+}
+export function emptyStyle(): Style {
+    return {articleType: "", baseColour: "", gender: "", masterCategory: "", productDisplayName: "", season: "", subcategory: "", usage: "", year: NaN}
+}
