973b1ae8457c9914d4ba4aed14d7f8c88a114ec7
cainsusk <cain@susko.ca>
2023-11-25 18:56:15 -0500

    chore: update gitignore to exclude fashion-dataset directory
    Additionally, adds firebaseConfig.js as well as node modules for creating hashes.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // import {...} from "firebase/auth";
-// import {...} from "firebase/database";
+import {getDatabase} from "firebase/database";
 import {getFirestore} from "firebase/firestore";
 import {get} from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 // import {...} from "firebase/functions";
@@ -19,10 +19,11 @@ const firebaseConfig = {
   storageBucket: "team-alex-app-cf42d.appspot.com",
   messagingSenderId: "938646970070",
   appId: "1:938646970070:web:961b8c64c0bf33e8943377",
-  measurementId: "G-RHGTVMK8Z7"
+  measurementId: "G-RHGTVMK8Z7",
+  databaseUrl: "https://team-alex-app-cf42d-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+export const db = getDatabase(app);
